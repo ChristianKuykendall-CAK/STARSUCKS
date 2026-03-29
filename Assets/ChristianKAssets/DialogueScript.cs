bcfdf7b47880acf53695a096f217369403943a93
CAKECHALICE <cak44@pct.edu>
2026-03-28 22:07:09 -0400

    added button scripts and dialogue script
    When hovering over the button it should play an animation. When clicking on the button it should go to the dialogue scene. In dialogue scene the script should be used when the button is pressed to continue the dialogue between the two characters

---
+++ Assets/ChristianKAssets/DialogueScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueScript : MonoBehaviour
+{
+    public TMP_Text dialogueTextBox;
+
+    int dialoguecounter = 0;
+
+    public void Counter(){
+        dialoguecounter++;
+    }
+    void Update(){
+        if(dialoguecounter == 0){
+            dialogueTextBox.text = "Cassie:\nOH MY GOSH!!! A REAL VAMPIRE";
+        }
+        else if(dialoguecounter == 1){
+            dialogueTextBox.text = "Jackie Daytona:\nWait? Your not afraid of me?";
+        }
+        else if(dialoguecounter == 2){
+            dialogueTextBox.text = "Cassie:\nOf course not. It was my lifelong dream to meet a real vampire.";
+        }
+        else if(dialoguecounter == 3){
+            dialogueTextBox.text = "Jackie Daytona:\nIn that case do you want to start working for me at my cafe.";
+        }
+        else if(dialoguecounter == 4){
+            dialogueTextBox.text = "Cassie:\nI absolutely will!";
+        }
+
+    }
+}
