cffaf4144c6002fd8aaab7f160b7c81cb8883775
hdp7 <hdp7@pct.edu>
2026-04-07 09:34:01 -0400

    Updated Patient

--- Assets/PatientController.cs
+++ Assets/PatientController.cs
@@ -1,11 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class PatientController : MonoBehaviour
 {
-    private bool isSelected = false;
+    public bool isSelected = false;
     public string patientName;
     public string bloodType;
+    private Transform patient;
+    private Quaternion origRot;
+    public int speed = 10;
+    public float angle = 15f;
 
+
+
+    private void Start()
+    {
+        patient = gameObject.GetComponent<Transform>();
+        origRot = patient.rotation;
+    }
     private void OnMouseDown()
     {
         isSelected = !isSelected;
@@ -17,6 +29,20 @@ public class PatientController : MonoBehaviour
         else
         {
             FormController.instance.StartCoroutine("RemovePage");
+        }
+    }
+
+     void Update()
+    {
+        if (!isSelected) 
+        { 
+            //wobble
+            float zOffset = Mathf.Sin(Time.time * speed) * angle;
+            patient.rotation = Quaternion.Euler(0, 0, origRot.z + zOffset);
+        }
+        else
+        {
+            patient.rotation = origRot;
         }
     }
 }
