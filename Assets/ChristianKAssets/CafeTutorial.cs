c2d27078b9ddcfafdb30062845b5f38151f1719e
CAKECHALICE <cak44@pct.edu>
2026-04-12 13:39:07 -0400

    Cafe Tutorial Finished
    Now it needs to work with the game manager so it doesn't pop up again later in the game.

---
+++ Assets/ChristianKAssets/CafeTutorial.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CafeTutorial : MonoBehaviour
+{
+    public GameObject[] parts;
+    int counter = 0;
+    bool tutorialFinished = false;
+    public GameObject MainGame;
+
+
+    public void NextStep()
+    {
+        if (counter < parts.Length)
+        {
+            Debug.Log(counter);
+
+            parts[counter].gameObject.SetActive(false);
+            counter++;
+            if (counter < parts.Length)
+            {
+                parts[counter].gameObject.SetActive(true);
+            }
+        }
+    }
+    public void FinishTutorial()
+    {
+        NextStep();
+        tutorialFinished = true;
+        MainGame.gameObject.SetActive(true);
+    }
+}
